6c15859b0f5a41910b5f0c503004d5a11fdf1eb8
XieRW <1429382875@qq.com>
2021-08-05 16:06:41 +0800

    init sass

--- src/App.vue
+++ src/App.vue
@@ -14,13 +14,15 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+  $color: #f00;
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  /*color: #2c3e50;*/
+  color: $color;
   margin-top: 60px;
 }
 </style>
