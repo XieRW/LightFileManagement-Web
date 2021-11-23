3be8fc83af32441e251976875491459bc910a4d0
XieRW <1429382875@qq.com>
2021-11-23 18:40:13 +0800

    init sass

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@ export default {
     HelloWorld
   }
 }
+
 </script>
 
 <style lang="scss">
--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+
 createApp(App).mount('#app')
+
+// // 若是没有开启Devtools工具，在开发环境中开启，在生产环境中关闭
+// if (process.env.NODE_ENV === 'development') {
+//     Vue.config.devtools = true;
+// } else {
+//     Vue.config.devtools = false;
+// }
+
